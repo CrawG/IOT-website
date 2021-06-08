03d58e9e80f663d9b2c2ad70bb071fa22312efde
Crawford Smith <crawfordgsmith@gmail.com>
2021-06-08 17:37:31 -0400

    videos added

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,12 +7,19 @@ export const Services = (props) => {
       <div className='container'>
         <div className='section-title'>
           <h2>Media</h2>
-          
         </div>
-        <div className="App">
-      {/* <h1>Youtube Embed</h1> */}
-      <YoutubeEmbed embedId="3s53G3R_QJA" />
-    </div>
+        <div className="row"> 
+          <div className="App col-md-5 col-md-offset-1">
+            {/* <h1>Youtube Embed</h1> */}
+              <YoutubeEmbed embedId="3s53G3R_QJA" />
+          </div>
+          <div></div>
+          <div className="App col-md-5">
+            {/* <h1>Youtube Embed</h1> */}
+              <YoutubeEmbed embedId="d71U0hnB6FE" />
+          </div>
+        </div>
+    
         {/* <div className='row'>
           {props.data
             ? props.data.map((d, i) => (
