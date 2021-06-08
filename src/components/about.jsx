6ec288b831282d2c0896d163cb76fb4603969ba8
Crawford Smith <crawfordgsmith@gmail.com>
2021-06-08 14:19:58 -0400

    update Media section, fix to About section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,22 +1,27 @@
 export const About = (props) => {
   return (
-    <div id='about'>
+  <div id='about' className='text-center'>  
       <div className='container'>
+        <div className='section-title'>
+          <h2>About</h2>
+
+    {/* <div id='about'> */}
+      {/* <div className='container'> */}
         {/* <div className='row'> */}
           {/* <div className='col-xs-12 col-md-6'>
             {' '}
             <img src='img/about.jpg' className='img-responsive' alt='' />{' '}
           </div> */}
-          <div className='col-xs-12 col-md- '>
-            <div className='section-title text-center'>
-              <h2>About Us</h2>
+          {/* <div className='col-xs-12 col-md- '> */}
+            {/* <div className='section-title text-center'> */}
+              {/* <h2>About Us</h2> */}
               <div>{props.data ? props.data.paragraph.map((d, i) => (
                           <p key={`${d}-${i}`}>{d}</p>
                         ))
                       : 'loading'}
               </div>
               <h3>Crawford Smith - Founder</h3>
-              <div className='list-style'>
+              {/* <div className='list-style'>
                 <div className='col-lg-6 col-sm-6 col-xs-12'>
                   <ul>
                     {props.data
@@ -25,8 +30,8 @@ export const About = (props) => {
                         ))
                       : 'loading'}
                   </ul>
-                </div>
-                <div className='col-lg-6 col-sm-6 col-xs-12'>
+                </div> */}
+                {/* <div className='col-lg-6 col-sm-6 col-xs-12'>
                   <ul>
                     {props.data
                       ? props.data.Why2.map((d, i) => (
@@ -34,12 +39,12 @@ export const About = (props) => {
                         ))
                       : 'loading'}
                   </ul>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        // </div>
+      // </div>
     // </div>
   )
 }
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -3,13 +3,10 @@ export const Services = (props) => {
     <div id='services' className='text-center'>
       <div className='container'>
         <div className='section-title'>
-          <h2>Our Services</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
-          </p>
+          <h2>Media</h2>
+          
         </div>
-        <div className='row'>
+        {/* <div className='row'>
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className='col-md-4'>
@@ -22,7 +19,7 @@ export const Services = (props) => {
                 </div>
               ))
             : 'loading'}
-        </div>
+        </div> */}
       </div>
     </div>
   )
